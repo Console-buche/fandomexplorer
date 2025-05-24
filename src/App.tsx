721893af2b7af5efab13481971d4d163029ae646
console-buche <dubourg.sebastien@gmail.com>
2025-05-25 00:27:31 +0200

    feat: added log

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './style/index.css';
 function App() {
   const queryClient = useQueryClient();
 
+  console.log('hello world!');
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className={layoutStyle}>
